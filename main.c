e4e63c0bc11fb3531c85b92405b0534671cbba89
Denis Konovalov <den.konovaloff@gmail.com>
2018-10-18 16:19:57 +0400

    Добавил данные из методички

--- main.c
+++ main.c
@@ -1,17 +1,43 @@
 #include "Main.h"
+#include <malloc.h>
 #include <stdio.h>
 #include <locale.h>
 
+typedef int T;
+FILE * file;
+
 typedef struct Node {
-	int data;
-	struct Node * left;
-	struct Node * right;
-	struct Node * parent;
+	T data;
+	struct Node *left;
+	struct Node *right;
+	struct Node *parent;
 } Node;
+
+// Построить идеально сбалансированное дерево с n узлами
+Node *Tree ( int n)
+{
+	Node *newNode;
+	int x , nl , nr;
+
+	if ( n == 0 )
+		newNode = NULL;
+	else
+	{
+		x = n+n;
+		nl = n / 2;
+		nr = n - nl - 1;
+		newNode = ( Node*) malloc ( sizeof ( Node ));
+		newNode -> data = x;
+		newNode -> left = Tree ( nl );
+		newNode -> right = Tree ( nr );
+	}
+
+	return newNode;
+}
 
 // Распечатка двоичного дерева в виде скобочной записи
 void printTree (Node *root ) {
-	if ( root)
+	if (root)
 	{
 		printf ( "%d" , root -> data );
 		if ( root -> left || root -> right)
@@ -35,13 +61,29 @@ void printTree (Node *root ) {
 	}
 }
 
+void preOrderTravers ( Node *root ) {
+	if ( root ) {
+		printf ( "%d " , root -> data );
+
+		preOrderTravers ( root -> left );
+		preOrderTravers ( root -> right );
+	}
+}
+
 int main(int argc, const char * argv[]){
+	Node* tree = NULL;
 	int x = 0;
 
 	setlocale(LC_ALL, "Rus");
 
+	printf("Введите количество узлов для дерева:\n");
+	scanf("%d", &x);
+	printf("\n");
 
+	tree = Tree(x);
+	printTree(tree);
 
+	printf("\n");
 	system("pause");
 	return 0;
 }
