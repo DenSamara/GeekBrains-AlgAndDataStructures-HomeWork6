ef001d53b5a6667127e8b6d5d0dff0000ba96210
Denis Konovalov <den.konovaloff@gmail.com>
2018-10-18 15:49:18 +0400

    Добавил данные из методички

--- main.c
+++ main.c
@@ -2,6 +2,38 @@
 #include <stdio.h>
 #include <locale.h>
 
+typedef struct Node {
+	int data;
+	struct Node * left;
+	struct Node * right;
+	struct Node * parent;
+} Node;
+
+// Распечатка двоичного дерева в виде скобочной записи
+void printTree (Node *root ) {
+	if ( root)
+	{
+		printf ( "%d" , root -> data );
+		if ( root -> left || root -> right)
+		{
+			printf ( "(" );
+
+			if ( root -> left )
+				printTree ( root -> left );
+			else
+				printf ( "NULL" );
+
+			printf ( "," );
+
+			if ( root -> right )
+				printTree ( root -> right );
+			else
+				printf ( "NULL" );
+
+			printf ( ")" );
+		}
+	}
+}
 
 int main(int argc, const char * argv[]){
 	int x = 0;
