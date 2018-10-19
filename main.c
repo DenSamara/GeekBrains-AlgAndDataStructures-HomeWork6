7d34fb383ebbded9690d0b5dbb734e52ac5d3ac6
Denis Konovalov <den.konovaloff@gmail.com>
2018-10-19 13:01:13 +0400

    Добавил решение задачи 1. Собственная Hash-функция

--- main.c
+++ main.c
@@ -2,9 +2,11 @@
 #include <malloc.h>
 #include <stdio.h>
 #include <locale.h>
+#include <math.h>
 
 typedef int T;
 FILE * file;
+static int const ARRAY_LENGTH = 30;
 
 typedef struct Node {
 	T data;
@@ -70,12 +72,53 @@ void preOrderTravers ( Node *root ) {
 	}
 }
 
+//Будем накапливать дробную часть от деления счётчика на код символа
+unsigned long long int hash(char data[], int arraylength){
+	unsigned long long int result = 0;
+	int i, charCode = 0;
+	double tmp = 0.0, s;
+	for(i = 0; i < arraylength; i++){
+		charCode = (int)data[i];
+		if (charCode > 0)
+			tmp += ((double)(i+1)/(double)charCode );
+	}
+
+	//Допустим, нам нужны 16 знаков
+	s = pow(10, 15);
+
+	//Отбрасываем дробную часть и получаем число
+	result = (unsigned long long int)(tmp*s);
+	return result;
+}
+
+void printHash(char data[]){
+	int i = 0;
+	char c = ' ';
+	while (c != '\n'){
+		c = data[i];
+		printf("%c", c);
+		i++;
+	}
+	printf("\n");
+}
+
 int main(int argc, const char * argv[]){
 	Node* tree = NULL;
 	int x = 0;
+	char data[] = {'S', 'i', 'm', 'p', 'l', 'e', ' ', 'd', 'a', 't', 'a', ' ', 't', 'o', ' ', 'd', 'e', 'm', 'o', 'n', 's', 't', 'r', 'a', 't', 'i', 'o', 'n', '\n'};
 
 	setlocale(LC_ALL, "Rus");
 
+	//--== Задание 1 ==--
+	// Реализовать простейшую хэш-функцию. На вход функции подается строка, на выходе сумма кодов символов.
+	printf("Данные для хеширования: ");
+	printHash(data);
+
+	printf("Hash = %llu\n", hash(data, ARRAY_LENGTH));
+	printf("\n");
+
+	//--== Задание 2 ==--
+	// Реализовать двоичное дерево поиска
 	printf("Введите количество узлов для дерева:\n");
 	scanf("%d", &x);
 	printf("\n");
